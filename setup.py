d25e2ba8043d632660a34ce4cc1798669747dfa8
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-11-12 15:31:55 +0100

    hmm>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Singer.io tap for extracting data from the Microsoft Dynamics 365 API",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     install_requires=["requests==2.22.0", "singer-python==5.8.1", "odata"],
-    dependency_links=["git+https://github.com/tuomur/python-odata.git#egg=odata-0.3"],
+    dependency_links=["https://github.com/tuomur/python-odata.git#egg=odata"],
     entry_points="""
           [console_scripts]
           tap-dynamics=tap_dynamics:main
