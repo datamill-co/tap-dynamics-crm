eedcd64560ffebf86b78ee40aebfd58810a95b3d
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-11-12 14:02:14 +0100

    clean up setup.py a bit

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@
 
 from setuptools import setup
 
-setup(name='tap-dynamics',
-      version='0.0.1',
-      description='Singer.io tap for extracting data from the Microsoft Dynamics 365 API',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_deputy'],
-      install_requires=[
-            'requests==2.22.0',
-            'singer-python==5.8.1'
-      ],
-      entry_points='''
+setup(
+    name="tap-dynamics",
+    version="0.0.1",
+    description="Singer.io tap for extracting data from the Microsoft Dynamics 365 API",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    install_requires=["requests==2.22.0", "singer-python==5.8.1"],
+    entry_points="""
           [console_scripts]
-          tap-dynamics-crm=tap_dynamics:main
-      ''',
-      packages=['tap_dynamics']
+          tap-dynamics=tap_dynamics:main
+      """,
+    packages=["tap_dynamics"],
 )
