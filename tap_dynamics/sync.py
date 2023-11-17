758d1f60cd951ccf9418feebece718330432180c
JingLin0 <jinglin.daisy@gmail.com>
2023-11-17 23:24:56 +0100

    fix bug of not getting all data

--- tap_dynamics/sync.py
+++ tap_dynamics/sync.py
@@ -57,34 +57,26 @@ def sync_stream(service, state, start_date, stream, mdata):
 
     schema = stream.schema.to_dict()
 
-    count = 0
-    with metrics.http_request_timer(stream.tap_stream_id):
-        with metrics.record_counter(stream.tap_stream_id) as counter:
-            for record in query:
-                dict_record = {}
-                for odata_prop in entitycls.__odata_schema__["properties"]:
-                    prop_name = odata_prop["name"]
-                    value = getattr(record, prop_name)
-                    if isinstance(value, datetime):
-                        value = singer.utils.strftime(value)
-                    dict_record[prop_name] = value
+    with metrics.record_counter(stream.tap_stream_id) as counter:
+        for record in query:
+            dict_record = {}
+            for odata_prop in entitycls.__odata_schema__["properties"]:
+                prop_name = odata_prop["name"]
+                value = getattr(record, prop_name)
+                if isinstance(value, datetime):
+                    value = singer.utils.strftime(value)
+                dict_record[prop_name] = value
+            with Transformer() as transformer:
+                dict_record = transformer.transform(dict_record, schema, mdata)
+            singer.write_record(stream.tap_stream_id, dict_record)
+            counter.increment()
 
-                if MODIFIED_DATE_FIELD in dict_record:
-                    if dict_record[MODIFIED_DATE_FIELD] > max_modified:
-                        max_modified = dict_record[MODIFIED_DATE_FIELD]
-                    else:
-                        continue
-
-                with Transformer() as transformer:
-                    dict_record = transformer.transform(dict_record, schema, mdata)
-                singer.write_record(stream.tap_stream_id, dict_record)
-                counter.increment()
-
-                count += 1
-                if count % 5000 == 0:
+            if MODIFIED_DATE_FIELD in dict_record:
+                if dict_record[MODIFIED_DATE_FIELD] > max_modified:
+                    max_modified = dict_record[MODIFIED_DATE_FIELD]
                     write_bookmark(state, stream_name, max_modified)
 
-    write_bookmark(state, stream_name, max_modified)
+        write_bookmark(state, stream_name, max_modified)
 
 
 def _sync_stream_incremental(service, entitycls, start):
